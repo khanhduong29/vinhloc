9d45b5111d118a6ba31a63f989d72ac2c785630c
minh200x <minhbs200x@gmail.com>
2020-06-25 08:54:09 +0700

    update service

--- application/views/client/service.php
+++ application/views/client/service.php
@@ -14,48 +14,50 @@
 							<div class="border-1 p-2 banner-sv">
 								<img src="/vinhloc/theme/frontend/images/slide/slide3.jpg" alt="">
 							</div>
-							<h3>
-								<span>CUNG CẤP DỊCH VỤ CHUYÊN NGHIỆP, HIỆN ĐẠI</span>
-							</h3>
-							<p>
-								Quý khách đang đi tìm một địa chỉ uy tín, cung cấp các sản phẩm hiện đại, mang đậm phong cách Châu Âu? Hãy đến với Vĩnh Lộc Luxury, công ty chúng tôi luôn có những sản phẩm mới nhất, tốt nhất, độc đáo nhất khi mà trên thị trường chưa xuất hiện.
-							</p>
-							<p>
-								Khi Quý khách đặt hàng tại <a href="index.html" >vinhlocluxury.com</a>, Quý khách sẽ được vận chuyển và lắp đặt miễn phí hoàn toàn. Những đơn hàng nội thành, chỉ mất chưa đầy 30 phút, hàng sẽ có sẵn tới địa chỉ quý khách muốn. Đội ngũ nhân sự sẽ giúp quý khách lắp đặt hệ thống đèn chuyên nghiệp. Từng chi tiết khi lắp như: hướng đèn, vị trí đặt đèn, quy cách bảo quản sẽ được nhân viên tư vấn nhiệt tình.
-							</p>
-							<h3>
-								<span>ĐỘI NGŨ NHÂN SỰ GIÀU KINH NGHIỆM, NHIỆT TÌNH, SÁNG TẠO</span>
-							</h3>
-							<p>Sự chuyên nghiệp không dừng lại ở các sản phẩm của Vĩnh Lộc mà con người nơi đây cũng sẽ khiến quý khách cảm nhận được điều đó. Đội ngũ nhân viên kinh doanh sẽ tư vấn cho bạn những mẫu đèn nào đang được thịnh hành nhất, quý khách nên chọn loại đèn nào để phù hợp với căn phòng của mình. Nhân viên kỹ thuật là người sẽ hoàn thiện giúp quý khách những công trình, những tuyệt tác đẹp nhất, độc nhất.</p>
-							<h3>
-								<span>CUNG CẤP DỊCH VỤ THI CÔNG, TRANG TRÍ ĐÈN NỘI THẤT TẠI NHÀ, TẠI CÔNG TRÌNH</span>
-							</h3>
-							<div class="contain-sr">
-								<div class="img-right">
-									<img src="/vinhloc/theme/frontend/images/product/2e3d45f2764a9014c95b.jpg" alt="">
+							<div class="s-content">
+								<h3>
+									<span>CUNG CẤP DỊCH VỤ CHUYÊN NGHIỆP, HIỆN ĐẠI</span>
+								</h3>
+								<p>
+									Quý khách đang đi tìm một địa chỉ uy tín, cung cấp các sản phẩm hiện đại, mang đậm phong cách Châu Âu? Hãy đến với Vĩnh Lộc Luxury, công ty chúng tôi luôn có những sản phẩm mới nhất, tốt nhất, độc đáo nhất khi mà trên thị trường chưa xuất hiện.
+								</p>
+								<p>
+									Khi Quý khách đặt hàng tại <a href="index.html" >vinhlocluxury.com</a>, Quý khách sẽ được vận chuyển và lắp đặt miễn phí hoàn toàn. Những đơn hàng nội thành, chỉ mất chưa đầy 30 phút, hàng sẽ có sẵn tới địa chỉ quý khách muốn. Đội ngũ nhân sự sẽ giúp quý khách lắp đặt hệ thống đèn chuyên nghiệp. Từng chi tiết khi lắp như: hướng đèn, vị trí đặt đèn, quy cách bảo quản sẽ được nhân viên tư vấn nhiệt tình.
+								</p>
+								<h3>
+									<span>ĐỘI NGŨ NHÂN SỰ GIÀU KINH NGHIỆM, NHIỆT TÌNH, SÁNG TẠO</span>
+								</h3>
+								<p>Sự chuyên nghiệp không dừng lại ở các sản phẩm của Vĩnh Lộc mà con người nơi đây cũng sẽ khiến quý khách cảm nhận được điều đó. Đội ngũ nhân viên kinh doanh sẽ tư vấn cho bạn những mẫu đèn nào đang được thịnh hành nhất, quý khách nên chọn loại đèn nào để phù hợp với căn phòng của mình. Nhân viên kỹ thuật là người sẽ hoàn thiện giúp quý khách những công trình, những tuyệt tác đẹp nhất, độc nhất.</p>
+								<h3>
+									<span>CUNG CẤP DỊCH VỤ THI CÔNG, TRANG TRÍ ĐÈN NỘI THẤT TẠI NHÀ, TẠI CÔNG TRÌNH</span>
+								</h3>
+								<div class="contain-sr">
+									<div class="img-right">
+										<img src="/vinhloc/theme/frontend/images/product/2e3d45f2764a9014c95b.jpg" alt="">
+									</div>
+									<div class="word-left">
+										Những công trình chúng tôi thi công chính là bằng chứng xác thực nhất để khẳng định chất lượng của Vĩnh Lộc. Quý khách sẽ bắt gặp bất cứ một công trình trang trí đèn nội thất nào đó, nhưng nó chỉ là vẻ đẹp thông thường do bộ đèn ấy mang lại thôi mà không thể nhìn được nghệ thuật toát lên từ những tác phẩm đó. Nhưng đến với Vĩnh Lộc Luxury, ngắm những công trình chúng tôi thi công, chắc chắn quý khách sẽ cảm thấy hoàn toàn khác biệt so với những bên khác làm. Quý khách hãy ghé thăm công trình của chúng tôi để biết thêm về những thành công mà chúng tôi đạt được.
+									</div>
 								</div>
-								<div class="word-left">
-									Những công trình chúng tôi thi công chính là bằng chứng xác thực nhất để khẳng định chất lượng của Vĩnh Lộc. Quý khách sẽ bắt gặp bất cứ một công trình trang trí đèn nội thất nào đó, nhưng nó chỉ là vẻ đẹp thông thường do bộ đèn ấy mang lại thôi mà không thể nhìn được nghệ thuật toát lên từ những tác phẩm đó. Nhưng đến với Vĩnh Lộc Luxury, ngắm những công trình chúng tôi thi công, chắc chắn quý khách sẽ cảm thấy hoàn toàn khác biệt so với những bên khác làm. Quý khách hãy ghé thăm công trình của chúng tôi để biết thêm về những thành công mà chúng tôi đạt được.
-								</div>
-							</div>
-							<h3>
-								<span>CẢM NHẬN CỦA KHÁCH HÀNG VỀ DỊCH VỤ CỦA VĨNH LỘC LUXURY</span>
-							</h3>
-							<div class="contain-sr">
-								<div class="img-right">
-									<img src="/vinhloc/theme/frontend/images/product/0fcdb1b05c06ba58e317.jpg" alt="">
-								</div>
-								<div class="word-left">
-									<p>Trong số các dự án Vĩnh Lộc thi công, một câu nói của vị khách hàng đã giúp chúng tôi có thêm động lực và phát triển như hôm nay. Mr.Đoàn – Tổng giám đốc Phú Thái Group VT 15 – Ecopark bày tỏ cảm xúc:</p>
-									<p class="font-italic">
-										“Một tác phẩm nghệ thuật cần nổi bật lên cái hồn của người nghệ sỹ. Những người làm nên một tuyệt tác để đời đáng được trân trọng. Vĩnh Lộc – đơn vị đã giúp tôi hoàn thành một tác phẩm nghệ thuật. Theo cá nhân tôi nghĩ, nếu cứ giữ vững phong độ này hoặc hơn thế nữa, chắc chắn Vĩnh Lộc sẽ không chỉ dừng lại ở những nghệ thuật trong nước mà còn vươn tầm ra khu vực.”
-									</p>
-									<p>
-										Đó là lời bày tỏ cảm xúc rất chân thành của Mr Đoàn, còn rất nhiều những cá nhân và tập thể khác bày tỏ niềm hạnh phúc khi được hợp tác với Vĩnh Lộc. Cùng đến với thế giới đèn của chúng tôi để cảm nhận được sự kho tàng nghệ thuật trong từng tác phẩm nhé.
-									</p>
-									<p>
-										Chúc quý khách mạnh khỏe và thành công!
-									</p>
+								<h3>
+									<span>CẢM NHẬN CỦA KHÁCH HÀNG VỀ DỊCH VỤ CỦA VĨNH LỘC LUXURY</span>
+								</h3>
+								<div class="contain-sr">
+									<div class="img-right">
+										<img src="/vinhloc/theme/frontend/images/product/0fcdb1b05c06ba58e317.jpg" alt="">
+									</div>
+									<div class="word-left">
+										<p>Trong số các dự án Vĩnh Lộc thi công, một câu nói của vị khách hàng đã giúp chúng tôi có thêm động lực và phát triển như hôm nay. Mr.Đoàn – Tổng giám đốc Phú Thái Group VT 15 – Ecopark bày tỏ cảm xúc:</p>
+										<p class="font-italic">
+											“Một tác phẩm nghệ thuật cần nổi bật lên cái hồn của người nghệ sỹ. Những người làm nên một tuyệt tác để đời đáng được trân trọng. Vĩnh Lộc – đơn vị đã giúp tôi hoàn thành một tác phẩm nghệ thuật. Theo cá nhân tôi nghĩ, nếu cứ giữ vững phong độ này hoặc hơn thế nữa, chắc chắn Vĩnh Lộc sẽ không chỉ dừng lại ở những nghệ thuật trong nước mà còn vươn tầm ra khu vực.”
+										</p>
+										<p>
+											Đó là lời bày tỏ cảm xúc rất chân thành của Mr Đoàn, còn rất nhiều những cá nhân và tập thể khác bày tỏ niềm hạnh phúc khi được hợp tác với Vĩnh Lộc. Cùng đến với thế giới đèn của chúng tôi để cảm nhận được sự kho tàng nghệ thuật trong từng tác phẩm nhé.
+										</p>
+										<p>
+											Chúc quý khách mạnh khỏe và thành công!
+										</p>
+									</div>
 								</div>
 							</div>
 						</div>
