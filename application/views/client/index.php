850125ef3d36556f68efafe828a3345f46aeca85
khanhduong29 <tran.khanhduong.566@gmail.com>
2020-07-03 09:35:23 +0700

    Update index.php

--- application/views/client/index.php
+++ application/views/client/index.php
@@ -33,7 +33,7 @@
                             			<img src="<?php echo public_url() ?>images/thumbs-cat/untitled-1.png" alt="">
                             		</div>
                             		<h3 class="m-0">
-                            			<a href="" title="">Đèn chùm</a>
+                            			<a href="" title=""><?php echo $pro['name'] ?></a>
                             		</h3>
                             	</div>
                             </div>
