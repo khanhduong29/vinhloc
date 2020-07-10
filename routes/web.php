9428297ac9f03234bc538220d95108822a52c4f6
khanhduong29 <tran.khanhduong.566@gmail.com>
2020-07-10 13:00:20 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -26,13 +26,6 @@ Route::group(['prefix' => '/'],function(){
     Route::get('cate-product','ClientController@cate_product')->name('cate-product');
 });
 
-
-Route::get('cart','CartController@cart')->name('cart');
-Route::get('checkout','CartController@checkout')->name('checkout');
-
-Route::get('login','LoginController@login')->name('login');
-Route::get('register','LoginController@register')->name('register');
-
 Route::group(['prefix' => 'admin','namespace'=>'admin'],function(){
 	// giao diện quản trị
 	Route::get('','adminController@admin')->name('admin');
