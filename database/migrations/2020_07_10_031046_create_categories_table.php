6a9d894b75af0e0d6f2f74e5b73a93f7b5a0050b
minh200x <minhbs200x@gmail.com>
2020-07-11 10:34:17 +0700

    table

--- database/migrations/2020_07_10_031046_create_categories_table.php
+++ database/migrations/2020_07_10_031046_create_categories_table.php
@@ -17,6 +17,7 @@ class CreateCategoriesTable extends Migration
             $table->increments('id');
             $table->string('slug');
             $table->string('name');
+            $table->string('image');
             $table->timestamps();
         });
     }
