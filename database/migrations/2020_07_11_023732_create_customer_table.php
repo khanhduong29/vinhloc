bd8825cbd124361be9bee6de5663773d604a5e80
khanhduong29 <tran.khanhduong.566@gmail.com>
2020-07-11 09:38:19 +0700

    update DB

--- database/migrations/2020_07_11_023732_create_customer_table.php
+++ database/migrations/2020_07_11_023732_create_customer_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateAdminTable extends Migration
+class CreateCustomerTable extends Migration
 {
     /**
      * Run the migrations.
@@ -30,6 +30,6 @@ class CreateAdminTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('admin');
+        Schema::dropIfExists('customer');
     }
 }
